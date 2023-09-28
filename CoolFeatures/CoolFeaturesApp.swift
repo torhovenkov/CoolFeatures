85a4d4b941b7235e71e4e0950c70851fbb87ee70
Vladyslav Torhovenkov <vladislav.lnx@gmail.com>
2023-09-28 23:48:25 +0300

    Initial Commit

---
+++ CoolFeatures/CoolFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolFeaturesApp.swift
+//  CoolFeatures
+//
+//  Created by Vladyslav Torhovenkov on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoolFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
