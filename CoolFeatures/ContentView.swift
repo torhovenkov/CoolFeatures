f2eb0fb9ead17a5fcd030da6c936f836c5be380c
Vladyslav Torhovenkov <vladislav.lnx@gmail.com>
2023-10-02 10:21:26 +0300

    Add custom bottom sheet to content view

--- CoolFeatures/ContentView.swift
+++ CoolFeatures/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isPresented = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            ImageBackground()
+            
+            MainButton() {
+                isPresented = true
+            }
         }
-        .padding()
+        .customSheet($isPresented, completion: { print("Finished") }) {
+            MediumContent()
+        }
     }
 }
 
